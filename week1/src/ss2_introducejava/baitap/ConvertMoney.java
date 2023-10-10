ec664c8e0f3b995d08a1d5d86b13a418d15aa899
Bảo Lê <levannhatbao29@gmail.com>
2023-10-10 14:34:11 +0700

    cap nhat

--- week1/src/ss2_introducejava/baitap/ConvertMoney.java
+++ week1/src/ss2_introducejava/baitap/ConvertMoney.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ConvertMoney {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double moneyVND = 23000;
+        double rate = 23000;
         double moneyUSD;
         System.out.println("input money USD ");
-        double convert = scanner.nextDouble();
-        moneyUSD = moneyVND*convert;
+        double moneyVND = scanner.nextDouble();
+        moneyUSD = rate*moneyVND;
         System.out.println("Số tiền đã chuyển đổi: "+moneyUSD+"VND");
     }
 }
--- week1/src/ss2_introducejava/baitap/ShowHello.java
+++ week1/src/ss2_introducejava/baitap/ShowHello.java
@@ -2,7 +2,7 @@ package ss2_introducejava.baitap;
 
 import java.util.Scanner;
 
-public class HienThiLoiChao {
+public class ShowHello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name? ");
