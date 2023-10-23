42ccb8be9e8ff56e204e2014c0a8eba4b1910324
Bảo Lê <levannhatbao29@gmail.com>
2023-10-23 21:59:39 +0700

    cap nhat

--- week1/src/ss10_stack/exc/bt2/CountTheNumber.java
+++ week1/src/ss10_stack/exc/bt2/CountTheNumber.java
@@ -1,4 +1,25 @@
 package ss10_stack.exc.bt2;
 
+import java.util.*;
+
 public class CountTheNumber {
+    public static void main(String[] args) {
+        Map<Character, Integer> integerMap = new TreeMap<>();
+        String string = "Bao Dep Trai";
+        String newString = string.toLowerCase();
+        String newContent = "";
+        for (int i = 0; i < newString.length(); i++) {
+            newContent += newString.charAt(i);
+        }
+        System.out.println(newContent);
+        for (int i = 0; i < newContent.length(); i++) {
+            if (integerMap.containsKey(newContent.charAt(i))) {
+                int count = integerMap.get(newContent.charAt(i));
+                integerMap.put(newContent.charAt(i), count + 1);
+            } else {
+                integerMap.put(newContent.charAt(i), 1);
+            }
+        }
+        System.out.println(integerMap);
+    }
 }
