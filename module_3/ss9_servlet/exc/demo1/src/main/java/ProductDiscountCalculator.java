e37fc6c128e11ecd94f78d7eb3ef2c0b4b11e2b9
Bảo Lê <levannhatbao29@gmail.com>
2023-11-28 20:42:01 +0700

    cap nhat

---
+++ module_3/ss9_servlet/exc/demo1/src/main/java/ProductDiscountCalculator.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ProductDiscountCalculator", value = "/calculator")
+public class ProductDiscountCalculator extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String description = req.getParameter("description");
+        double price = Float.parseFloat(req.getParameter("price"));
+        double discount = Float.parseFloat(req.getParameter("discount"));
+        double discountAmount = price * discount * 0.01;
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println(description);
+        writer.println("<h3> Sum: " + discountAmount + "</h3>");
+        writer.println("</html>");
+    }
+}
---
+++ module_3/ss9_servlet/exc/demo1/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JSP - Hello World</title>
+</head>
+<body>
+<h1 class="header__word">Ứng dụng Product Discount Calculator </h1>
+<form action="calculator" method="post">
+  <input type="text" name="description" placeholder="Product Description">
+  <br>
+  <input type="text" name="price" placeholder="List Price">
+  <br>
+  <input type="text" name="discount" placeholder="Discount Percent">
+  <br>
+  <button type="submit">Calculate Discount</button>
+</form>
+</body>
+</html>
